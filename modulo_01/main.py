323273fc02e0571c8ff6dd56608464f41b95504f
Ewerthon <ewerthonribeiro0@gmail.com>
2025-10-13 17:00:15 -0300

    módulo 2

---
+++ modulo_01/main.py
@@ -0,0 +1,19 @@
+'''
+Nomeclaturas
+PASCALCase
+CamelCase
+-> Snake Case <-
+Kebab-Case
+
+Comentários, podem ser usados como apostrofos ou cerquilhas
+
+
+Quais os elementos que compõem um programa?
+R: Instruções, expressões, exibições em tela.
+''' 
+
+# Exibe a mensagem na tela
+print("Hello World")
+
+# Pede para o usuário digitar o nome
+input("Muito bem, qual o seu nome?")
